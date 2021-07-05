a6ba11ddf40702b079f0c3cd67419cd5b03a2843
Dell <antiver@foxmail.com>
2021-07-05 13:30:17 +0800

    upload

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+import time
 
 PATH = "/root/torch_test/training/"
 dirs = [name for name in os.listdir(PATH)]
@@ -9,7 +9,11 @@ files = []
 while True:
     print(dirs)
     files_new = [name for name in os.listdir(os.path.join(PATH, dirs[-1], 'trainPics'))]
+    log = [name for name in os.listdir(os.path.join(PATH, dirs[-1], 'trainingLogs'))]
     difference = list(set(files_new) - set(files))
     if len(difference) > 0:
         process = subprocess.Popen(f"gupload {os.path.join(PATH, dirs[-1], 'trainPics', difference[0])}", shell=True, stdout=subprocess.PIPE)
+        process = subprocess.Popen(f"gupload {log[0]}", shell=True, stdout=subprocess.PIPE)
         process.wait()
+    files = files_new
+    time.sleep(300)
